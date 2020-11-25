b215414a783f2080c8fde98d3b674bd130bc65c5
Randy <54797571+RandyOfosu@users.noreply.github.com>
2020-11-25 13:28:35 +0100

    update inleveren

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,11 @@
 -- Opdracht 1
-
+UPDATE studenten SET woonplaats = 'Amstelveen';
 -- Opdracht 2 
-
+UPDATE studenten SET adres = 'somewhere', postcode = '1234AB' WHERE student_id >= 12;
 -- Opdracht 3
-
+UPDATE studenten SET geboortedatum = '2000-12-12' WHERE student_id = 2;
 -- Opdracht 4
-
+UPDATE studenten SET klas = '9C' WHERE student_id = 4;
+UPDATE studenten SET klas = '9A' WHERE student_id = 8;
 -- Opdracht 5
+UPDATE studenten SET voornaam = 'Johannah' WHERE student_id = 6;
--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,8 +1,8 @@
 -- Opdracht 1
-
+UPDATE studenten SET klas = REPLACE(klas, '9A', '9X');
 -- Opdracht 2 
-
+UPDATE studenten SET klas = REPLACE(klas, '9X', '9Z') WHERE student_id > 7; 
 -- Opdracht 3
-
+UPDATE studenten SET woonplaats = REPLACE(woonplaats, 'Amstelveen', 'Amsterdam') WHERE student_id BETWEEN 2 AND 7; 
 -- Opdracht 4
-
+UPDATE studenten SET woonplaats = REPLACE(woonplaats, 'Amsterdam', 'Uithoorn') WHERE klas = '9X';
